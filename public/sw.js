ffc380c4652ecf0a0756d47b1ded63a49d91de84
danikaneshov <danik.aneshov@gmail.com>
2026-04-05 23:37:04 +0500

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -4,9 +4,7 @@ const ASSETS = [ '/', '/index.html', '/manifest.json' ];
 self.addEventListener('install', e => {
     e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
-
 self.addEventListener('fetch', e => {
-    // Не кэшируем API запросы к бэкенду
     if (e.request.url.includes('/api/')) return;
     e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
